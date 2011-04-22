37eb46b50ed8d52d435a7d335f0b1ba5acd9e677
Andy Vanee <1andyvanee@gmail.com>
2011-04-22 01:06:02 -0600

    A couple basic tests added

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ window.onload = function(){
   stylesheet.type = "text/css";
   stylesheet.innerHTML = "body {background-color: #D0D5DA;} ";
   stylesheet.innerHTML += "#tests {font-family:'Helvetica', sans-serif; padding:22px; margin: 6px;} ";
-  stylesheet.innerHTML += "p.fadein{opacity: 0; font-size: 22px;} ";
+  stylesheet.innerHTML += "p.fadein{opacity: 0; font-size: 22px;} p.fadein:hover{color: #229} ";
   p("head").obj[0].appendChild(stylesheet);
 
   p("#tests").obj[0].innerHTML = "<h1>parts_js:</h1>";
@@ -21,9 +21,18 @@ app = {
   fadein: function(){
     (app.opacity < 0.95) ? app.opacity += 0.004 : clearInterval(app.interval);
     p(".fadein").cssText({"opacity": app.opacity});
-  }
+  },
+  counter: 0
 }
 
 app.tests = function(){
   (p("body").obj[0].toString() == "[object HTMLBodyElement]") ? 0 : console.log("Body selection failed");
+  (p('body').obj[0] == document.getElementsByTagName("body")[0]) ? 0 : console.log("Body identity test failed");
+  (p('#tests').obj[0] == document.getElementById("tests")) ? 0 : console.log("#tests identity test failed");
+  
+  app.counter=0;
+  console.log("p('.fadein').each found this: ");
+  p(".fadein").each(function(){app.counter += 1}).log();
+  (app.counter === 1) ? 0 : console.log("Error in each() function.");
+  
 }
