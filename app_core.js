e19f6304a4fdbeca0f08d1632c08bef876910d57
Andy Vanee <1andyvanee@gmail.com>
2011-04-22 22:00:57 -0600

    Fill out tests for parts_core a bit.

--- app_core.js
+++ app_core.js
@@ -6,14 +6,25 @@
  */
  
 window.onload = function(){
+  app.style();
   app.tests();
 }
 
-app = {
+app = {}
+
+app.tests = function(){
+  p("#tests").value[0].innerHTML = "<h1>parts_js: running</h1>";
+  p("#console").value[0].innerHTML = "<p>ID selection successful.</p>";
+  p("#console p").value[0].innerHTML += "<br>&nbsp;&nbsp;Descendant selection successful.";
+  p(".sidebar").value[0].innerHTML += "<p>Class selection successful.</p>";
 }
 
-app.tests = function(){  
-  p("#tests").value[0].innerHTML = "<h1>Parts: running</h1><p>ID selection successful.</p>"
-  p(".sidebar").value[0].innerHTML = "<p>Class selection successful</p>";
-  p("#console p").value[0].innerHTML += "<p>Descendant selection successful.</p>";
-}
+app.style = function(){
+  var stylesheet = document.createElement("style");
+  stylesheet.type = "text/css";
+  stylesheet.innerHTML = "body {background-color: #D0D5DA; font-family:'Helvetica', sans-serif;}";
+  stylesheet.innerHTML += "p {margin: 2px 10px;}";
+  stylesheet.innerHTML += "#tests { padding:3%; width: 60%; float: left;} ";
+  stylesheet.innerHTML += "#console{float:right; height: 60%; width: 28%; border:1px solid #AAA; color: #555; margin: 3%; overflow-y: auto;}";
+  p("head").value[0].appendChild(stylesheet);
+  }
